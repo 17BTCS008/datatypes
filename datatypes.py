1a995f54ca48999c927ab346de475bfdc1303090
17BTCS008 <35477449+17BTCS008@users.noreply.github.com>
2018-02-06 10:28:00 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+#finding square root
+"""
+import cmath                #complex math
+import math
+a=cmath.sqrt(-26)
+print(a)
+b=math.sqrt(26)
+print(int(b))               #to print integers"""
+
+
+#using dictionary(accessing)
+"""d={1:"Avinash","py":30}
+print(d[1])"""
+
+
+
+#list(datas can be modified)
+"""a=[6,4,4,5,7]
+a[0]=67
+a[1]=68
+a[2]=69
+print(a)"""
+
+
+
+#tuples(values cannot be modified or replaced)
+"""a=(1,2,3,4,5,6,7,8)
+print(a)"""
+
+
+#strings
+a="hi "
+b="Avinash"            #addition of two strings is called concatenation
+c=a+b
+print(c)
+
